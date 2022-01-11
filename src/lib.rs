b85e07cbfcaab3c41cfc8be88eb280291b1da103
tangjz <36634584@qq.com>
2022-01-11 23:49:15 +0800

    未写完的代码

--- src/lib.rs
+++ src/lib.rs
@@ -4,89 +4,100 @@ pub mod object_metadata;
 
 use async_trait::async_trait;
 
-use libunftp::storage::{Error, ErrorKind, Fileinfo, Metadata, StorageBackend};
-use reqwest::Client;
+use reqwest::header::HeaderValue;
+use tokio::io::AsyncRead;
+
+use libunftp::auth::UserDetail;
+use libunftp::storage::{Error, Fileinfo, Metadata, StorageBackend};
+use tokio_util::codec::{BytesCodec, FramedRead};
 use uri::HttpUri;
 use object_metadata::ObjectMetadata;
+use reqwest::{Body, Client, Url, header};
+use tokio::io::BufReader;
 use std::{
     fmt::Debug,
     path::{Path, PathBuf},
 };
 
+#[derive(Clone, Debug)]
 pub struct HttpFileSystem {
-    uri: HttpUri,
+    uris: HttpUri,
+    client: Client,
+    token: String,
 }
 
 impl HttpFileSystem {
-    // pub fn new<Str>(base_url: Str, bucket: Str) -> Self where Str: Into<String> {
-
-    // }
+    pub fn new<Str>(base_url: Str, bucket: Str, root: PathBuf, token: Str) -> Self where Str: Into<String> {
+        let uris = HttpUri::new(base_url.into(), bucket.into(), root);
+        let client = Client::new();
+        let token = token.into();
+        HttpFileSystem {
+            uris,
+            client,
+            token,
+        }
+    }
 }
 
 #[async_trait]
-impl<U: Sync + Send + Debug> StorageBackend<U> for HttpFileSystem {
+impl<User: UserDetail> StorageBackend<User> for HttpFileSystem {
     type Metadata = ObjectMetadata;
 
-    fn supported_features(&self) -> u32 {
-        libunftp::storage::FEATURE_SITEMD5
+    async fn metadata<P: AsRef<Path> + Send + Debug>(&self, _user: &User, _path: P) -> Result<Self::Metadata, Error> {
+        todo!()
     }
 
-    async fn metadata<P: AsRef<Path> + Send + Debug>(&self, _user: &Option<U>, path: P) -> Result<Self::Metadata, Error> {
-        // TODO: implement this
-        Err(Error::from(ErrorKind::CommandNotImplemented))
-    }
-
-    async fn list<P: AsRef<Path> + Send + Debug>(&self, _user: &Option<U>, path: P) -> Result<Vec<Fileinfo<PathBuf, Self::Metadata>>, Error>
+    async fn list<P: AsRef<Path> + Send + Debug>(&self, _user: &User, _path: P) -> Result<Vec<Fileinfo<PathBuf, Self::Metadata>>, Error>
     where
-        <Self as StorageBackend<U>>::Metadata: Metadata,
+        <Self as StorageBackend<User>>::Metadata: Metadata,
     {
-        // TODO: implement this
-        Err(Error::from(ErrorKind::CommandNotImplemented))
+        todo!()
     }
 
-    async fn get<P: AsRef<Path> + Send + Debug>(
-        &self,
-        _user: &Option<U>,
-        path: P,
-        _start_pos: u64,
-    ) -> Result<Box<dyn tokio::io::AsyncRead + Send + Sync + Unpin>, Error> {
-        // TODO: implement this
-        Err(Error::from(ErrorKind::CommandNotImplemented))
+    async fn get<P: AsRef<Path> + Send + Debug>(&self, _user: &User, _path: P, _start_pos: u64) -> Result<Box<dyn AsyncRead + Send + Sync + Unpin>, Error> {
+        todo!()
     }
 
-    async fn put<P: AsRef<Path> + Send + Debug, B: tokio::io::AsyncRead + Send + Sync + Unpin + 'static>(
+    async fn put<P: AsRef<Path> + Send + Debug, B: AsyncRead + Send + Sync + Unpin + 'static>(
         &self,
-        _user: &Option<U>,
+        _user: &User,
         bytes: B,
         path: P,
         _start_pos: u64,
     ) -> Result<u64, Error> {
-        // TODO: implement this
-        Err(Error::from(ErrorKind::CommandNotImplemented))
+        let url: Url = self.uris.put(path)?;
+
+        let client: Client = self.client.clone();
+
+        let reader = BufReader::with_capacity(4096, bytes);
+        
+        let token = self.token.clone();
+
+        let res = client.post(url)
+            .header(header::AUTHORIZATION, HeaderValue::from_str(format!("Bearer {}", token).as_str()).unwrap())
+            .header(header::CONTENT_TYPE, HeaderValue::from_static("application/octet-stream"))
+            .body(Body::wrap_stream(FramedRead::new(reader, BytesCodec::new())))
+            .send().await;
+            todo!()
     }
 
-    async fn del<P: AsRef<Path> + Send + Debug>(&self, _user: &Option<U>, path: P) -> Result<(), Error> {
-        // TODO: implement this
-        Err(Error::from(ErrorKind::CommandNotImplemented))
+    async fn del<P: AsRef<Path> + Send + Debug>(&self, _user: &User, _path: P) -> Result<(), Error> {
+        todo!()
     }
 
-    async fn mkd<P: AsRef<Path> + Send + Debug>(&self, _user: &Option<U>, path: P) -> Result<(), Error> {
-        // TODO: implement this
-        Err(Error::from(ErrorKind::CommandNotImplemented))
+    async fn mkd<P: AsRef<Path> + Send + Debug>(&self, _user: &User, _path: P) -> Result<(), Error> {
+        todo!()
     }
 
-    async fn rename<P: AsRef<Path> + Send + Debug>(&self, _user: &Option<U>, _from: P, _to: P) -> Result<(), Error> {
-        // TODO: implement this
-        Err(Error::from(ErrorKind::CommandNotImplemented))
+    async fn rename<P: AsRef<Path> + Send + Debug>(&self, _user: &User, _from: P, _to: P) -> Result<(), Error> {
+        todo!()
     }
 
-    async fn rmd<P: AsRef<Path> + Send + Debug>(&self, _user: &Option<U>, _path: P) -> Result<(), Error> {
-        // TODO: implement this
-        Err(Error::from(ErrorKind::CommandNotImplemented))
+    async fn rmd<P: AsRef<Path> + Send + Debug>(&self, _user: &User, _path: P) -> Result<(), Error> {
+        todo!()
     }
 
-    async fn cwd<P: AsRef<Path> + Send + Debug>(&self, _user: &Option<U>, _path: P) -> Result<(), Error> {
-        // TODO: Do we want to check here if the path is a directory?
-        Ok(())
+    async fn cwd<P: AsRef<Path> + Send + Debug>(&self, _user: &User, _path: P) -> Result<(), Error> {
+        todo!()
     }
 }
